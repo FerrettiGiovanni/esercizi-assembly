3c67e14560ea7869452289ad1a75e55bc16abf32
FerrettiGiovanni <ferrettigiovanni@protonmail.com>
2024-09-24 23:42:28 +0200

    Aggiornati i commenti nel codice Assembly per maggiore chiarezza

--- mips1.asm
+++ mips1.asm
@@ -4,13 +4,13 @@
 .globl main
 
 main:
-	lw $t1,pippo
-	lw $t2,paperino
-	add $t0,$t1,$t2
+	lw $t1,pippo # Carica il valore memorizzato nell'etichetta 'pippo' nel registro $t1
+	lw $t2,paperino # Carica il valore memorizzato nell'etichetta 'paperino' nel registro $t2
+	add $t0,$t1,$t2 # Somma i valori nei registri $t1 e $t2, salva il risultato in $t0
 	
-li $v0, 10
-syscall	
+li $v0, 10 # Carica il codice di sistema 10 (terminate il programma) nel registro $v0
+syscall	# Esegui la system call per terminare il programma
 
 .data 
-pippo: .word 4
-paperino: .word 6
+pippo: .word 4 # Definisce la variabile 'pippo' e assegna il valore 4
+paperino: .word 6 # Definisce la variabile 'paperino' e assegna il valore 6
